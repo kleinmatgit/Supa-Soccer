ad5179f2d65b10a85cf310f48854e4436ffb2509
kleinmatgit <kleinmateo@gmail.com>
2018-09-08 21:01:03 +0800

    centered standings and start adding images

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ const config = {
   module: {
     rules: [
       {test: /\.js$/, exclude: /node_modules/, use: 'babel-loader'},
-      {test: /\.css$/, use: ['style-loader','css-loader']}
+      {test: /\.css$/, use: ['style-loader','css-loader']},
     ]
   },
   node: {
