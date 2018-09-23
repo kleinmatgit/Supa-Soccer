1af19d3f9be288c6d3aa79f476c283bb919a2f6f
kleinmatgit <klein_math@yahoo.fr>
2018-09-23 16:11:18 +0800

    created production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 const config = {
   resolve: {
@@ -7,14 +8,39 @@ const config = {
       path.resolve('./node_modules'),
     ]
   },
-  entry: ['babel-polyfill', './lib/renderers/dom.js'],
+  entry : {
+    vendor: [
+      'babel-polyfill',
+      'jquery',
+      'react',
+      'react-dom',
+      'react-bootstrap',
+      'react-redux',
+      'redux',
+      'redux-immutable-state-invariant',
+      'redux-thunk',
+      'prop-types',
+      'request',
+      'toastr'
+    ],
+    app: ['./lib/renderers/dom.js'],
+  },
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   module: {
     rules: [
-      {test: /\.js$/, exclude: /node_modules/, use: 'babel-loader'},
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'env', 'stage-2']
+          }
+        }
+      },
       {test: /\.css$/, use: ['style-loader','css-loader']},
     ]
   },
@@ -24,6 +50,11 @@ const config = {
     net: 'empty',
     tls: 'empty'
   },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+    })
+  ]
 };
 
 module.exports = config;
